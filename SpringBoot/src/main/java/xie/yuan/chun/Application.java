b7ae5dd6fbeaef7ff07c17405b867baa88b8ecad
xieyuanchun <504799286@qq.com>
2019-05-21 21:48:09 +0800

    test

--- SpringBoot/src/main/java/xie/yuan/chun/Application.java
+++ SpringBoot/src/main/java/xie/yuan/chun/Application.java
@@ -15,6 +15,8 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
+	
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
